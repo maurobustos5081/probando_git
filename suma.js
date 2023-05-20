5ebbbfd9c971d3e86dc047b20aa5a23765187ef9
maurobustos5081 <mauro_18_b@hotmail.com>
2023-05-20 18:20:02 -0300

    se agrega funcion de resta

--- suma.js
+++ suma.js
@@ -1,7 +1,13 @@
-console.log("Aplicacion que suma");
+console.log('Aplicacion que suma')
 
-let suma = num1 + num2;
+let suma = num1 + num2
 
-console.log(suma);
+console.log(suma)
 
-console.log("Aplicacion que resta");
+console.log('Aplicacion que resta')
+
+let resta = function (num1, num2) {
+  return num1 - num2
+}
+
+console.log(resta)
